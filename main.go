e1e9d86f9790431826340d14ee82b9b1644171a0
Emiliano Santucci <emiliano.santucci@euforlegal.it>
2024-10-19 17:45:52 +0200

    🔊- Converted logs from standard lib to zap logger

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/solunion/way/internal"
 	"github.com/solunion/way/internal/app/api/application"
 	"github.com/solunion/way/internal/app/api/profile"
 	"github.com/solunion/way/internal/app/api/rule"
 	"github.com/solunion/way/internal/app/api/tenant"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -18,35 +19,39 @@ func main() {
 		tenant.Module,
 		application.Module,
 		fx.Invoke(simpleApp),
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
 	).Run()
 }
 
 func simpleApp(
+	log *zap.SugaredLogger,
 	tenantRepository tenant.TenantRepository,
 	applicationRepository application.ApplicationRepository,
 	profileRepository profile.ProfileRepository,
 	ruleRepository rule.RuleRepository,
 ) {
-	fmt.Println("App started!!!")
+	log.Debug("App started!!!")
 
 	var tenants []tenant.Tenant
 	err := tenantRepository.FindAll(&tenants)
-	fmt.Printf("Row affected: %d, Error: %s, Tenants: %v\n", len(tenants), err, tenants)
+	log.Debugf("Row affected: %d, Error: %s, Tenants: %v\n", len(tenants), err, tenants)
 
 	var applications []application.Application
 	err = applicationRepository.FindAll(&applications)
-	fmt.Printf("Row affected: %d, Error: %s, Applications: %v\n", len(applications), err, applications)
+	log.Debugf("Row affected: %d, Error: %s, Applications: %v\n", len(applications), err, applications)
 
 	var profiles []profile.Profile
 	err = profileRepository.FindAll(&profiles)
-	fmt.Printf("Row affected: %d, Error: %s, Profiles: %v\n", len(profiles), err, profiles)
+	log.Debugf("Row affected: %d, Error: %s, Profiles: %v\n", len(profiles), err, profiles)
 
 	var rules []rule.Rule
 	err = ruleRepository.FindAll(&rules)
-	fmt.Printf("Row affected: %d, Error: %s, Rules: %v\n", len(rules), err, rules)
+	log.Debugf("Row affected: %d, Error: %s, Rules: %v\n", len(rules), err, rules)
 
 	for _, r := range rules {
-		fmt.Printf("Rule[%q]: name=%q, description=%#v, ruleType={ name: %q }, tenantModel={ name:%q }\n",
+		log.Debugf("Rule[%q]: name=%q, description=%#v, ruleType={ name: %q }, tenantModel={ name:%q }\n",
 			r.ID,
 			r.Name,
 			r.Description,
