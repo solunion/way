a508fe0cab9c5f7fd48dc25c38e92c8e254d9077
Emiliano Santucci <emiliano.santucci@gmail.com>
2024-12-18 13:45:30 +0100

    ♻️- Change Nest.js app's name from 'way' to 'backend'

--- apps/backend-e2e/jest.config.ts
+++ apps/backend-e2e/jest.config.ts
@@ -1,14 +1,17 @@
 export default {
-  displayName: 'way-e2e',
+  displayName: 'backend-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
   setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    }],
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/way-e2e',
