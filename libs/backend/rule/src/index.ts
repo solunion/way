f218b63ff15bf8ef1334b21304ea93f0672960ea
Emiliano Santucci <emiliano.santucci@gmail.com>
2024-12-31 14:10:49 +0100

    ✨- Add rule lib + models

---
+++ libs/backend/rule/src/index.ts
@@ -0,0 +1,6 @@
+// export * from './lib/rule.entity';
+// export * from './lib/rule.repository';
+// export * from './lib/rule.service';
+// export * from './lib/rule-set.entity';
+// export * from './lib/rule-set.repository';
+// export * from './lib/rule-set.service'; 
