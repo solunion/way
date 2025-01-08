acf735d3f26743049a197fb3c4329e86b7488b7b
Simone Golia <s.golia@eustema.it>
2025-01-08 18:39:39 +0100

    ➕- Rebase

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NavbarComponent } from './shared/components/structural/navbar/navbar.component';
+import { ActionMenuComponent } from './shared/components/structural/action-menu/action-menu.component';
+import { ToolbarComponent } from './shared/components/structural/toolbar/toolbar.component';
+import { UtilityService } from './shared/services/utility.service';
+import { NgClass } from '@angular/common';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    NavbarComponent,
+    ActionMenuComponent,
+    ToolbarComponent,
+    NgClass
+  ],
   selector: 'way-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //Injection
+  #utilityService = inject(UtilityService);
+
+  //States
+  darkModeOn = signal( false );
+
   title = 'portal';
+
+  /**
+   *
+   */
+  ngOnInit() {
+    this.#utilityService.darkModeOn$
+      .subscribe((value : boolean) => {
+        this.darkModeOn.set( value );
+      });
+  }
 }
