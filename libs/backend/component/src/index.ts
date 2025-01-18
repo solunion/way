993950b108fddc41f98bfb5a8b1b0b9aec081c55
Emiliano Santucci <emiliano.santucci@gmail.com>
2025-01-18 15:46:45 +0100

    🩹- Remove unused export

--- libs/backend/component/src/index.ts
+++ libs/backend/component/src/index.ts
@@ -1,7 +1,3 @@
-export * from './lib/component.model';
 export * from './lib/component.module';
 export * from './lib/component.service';
 export * from './lib/component.controller';
-export * from './lib/dto/component.dto';
-export * from './lib/dto/create-component.dto';
-export * from './lib/dto/update-component.dto';
